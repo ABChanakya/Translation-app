64d9e573bb402b49bdc614b8add8a956a0313e29
Chanakya Bhaskara <s-cbhask@haw-landshut.de>
2026-04-01 08:18:20 +0200

    Add .gitignore and remove generated/local artifacts

--- .repo_archive/20260324_cleanup/root_legacy/data/utils/deleter.py
+++
@@ -1,6 +0,0 @@
-import torch, gc
-for obj in ['model', 'optimizer', 'data_loader']:
-	if obj in locals():
-		del locals()[obj]              # drop big objects you no longer need
-gc.collect()                           # run Python garbage collector
-torch.cuda.empty_cache()               # tell the allocator to release the cache
